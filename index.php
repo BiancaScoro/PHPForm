df757c1be40d86ec62962938e4549cd596d9aecd
Bianca Scoropan <biancascoro@gmail.com>
2024-04-23 09:39:52 -0400

    First release of Hello World

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Document</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <main>
+            <form action="includes/formhandler.php" method="post">
+                <label for="firstname">Firstname?</label>
+                <input id="firstname" type="text" name="firstname" placeholder="Firstname
+        <script src="" async defer></script>
+    </body>
+</html>
